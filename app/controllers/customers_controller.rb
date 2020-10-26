647fc61d15ba5676d62ecb0a8f7d9e13ad9f8705
acieagsalud <aciejohn@gmail.com>
2020-10-26 12:12:52 -0500

    Created customers controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,10 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def action
+  end
+
+  def view
+  end
+end
---
+++ test/controllers/customers_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CustomersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customers_index_url
+    assert_response :success
+  end
+
+  test "should get action" do
+    get customers_action_url
+    assert_response :success
+  end
+
+  test "should get view" do
+    get customers_view_url
+    assert_response :success
+  end
+
+end
